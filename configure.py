356cc368250804e047f62c0f0208f0f75c573b74
Bryce W <58643544+02bwilson@users.noreply.github.com>
2023-01-05 16:21:04 -0500

    various fixes

--- configure.py
+++ configure.py
@@ -4,7 +4,7 @@ from rtlsdr import *
 class SDR:
     _VERSION_ = "1.0"
 
-    def __init__(self, sample_rate=2e6, center_freq=1e7, freq_correction=45, gain='auto'):
+    def __init__(self, sample_rate=2e6, center_freq=95e6, freq_correction=45, gain='auto'):
 
         # Instantiate sdr object
         self.sdr_obj = RtlSdr()
@@ -12,8 +12,11 @@ class SDR:
         # Set params
         self.set_sample_rate(sample_rate)
         self.set_center_freq(center_freq)
-        self.set_center_freq(freq_correction)
+        self.set_freq_correction(freq_correction)
         self.set_gain(gain)
+
+    def read_samples(self, size):
+        return self.sdr_obj.read_samples(size)
 
     def close(self):
         self.sdr_obj.close()
@@ -27,7 +30,8 @@ class SDR:
     def set_center_freq(self, freq):
         try:
             self.sdr_obj.center_freq = freq
-        except:
+        except Exception as e:
+            print(e)
             print("Invalid Center Freq!")
 
     def set_freq_correction(self, rate):
--- test.py
+++ test.py
@@ -1,11 +1,27 @@
+import datetime
+import logging
+
 from configure import SDR
 from DataSample import sample
 
 
 def main():
+    logging.basicConfig(filename='logs/{}.log'.format(datetime.datetime.now().strftime("%m-%d-%y_%H-%M-%S")),
+                        encoding='utf-8', level=logging.DEBUG)
+
+
+    logging.debug("Getting SDR Object")
     sdr = SDR()
+    logging.debug("SDR Object {}".format(str(sdr)))
+    logging.debug("Getting sample getter")
     sample_getter = sample(sdr)
-    print(sample_getter.get(10))
+    logging.debug("Sample getter {}".format(sample_getter))
+
+    data = sample_getter.get(50)
+    print(data)
+    logging.debug("Got Data! - Size:{}".format(len(data)))
+
+
 
 
 if __name__ == "__main__":
